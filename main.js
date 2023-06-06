89b9cb6f89f024c905a9ba90211a1dd1ddf9ee81
Guilherme Santos <GuilhermeKorvoSant>
2023-06-05 21:29:28 -0300

    Adicionando data-atributes para começar a manipular as estatisticas do robô.

--- main.js
+++ main.js
@@ -1,17 +1,53 @@
-const somar = document.querySelector("#somar");
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
-controle.forEach((elemento) => {
-    elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
-    })
-});
-//Aqui estamos utilizando uma função anônima, ou seja, ela não tem nome. Também nomeada de 'arrow function', podemos utilizar: "() =>" 
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
 
-function manipulaDados (operacao) {
-    if(operacao === "-") {
-        braco.value = parseInt(braco.value) - 1
-    } else {
-        braco.value = parseInt(braco.value) + 1
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
     }
 }
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+    })
+})
+
+//Aqui estamos utilizando uma função anônima, ou seja, ela não tem nome. Também nomeada de 'arrow function', podemos utilizar: "() =>" 
+function manipulaDados (operacao, controle) {
+    const peca = controle.querySelector("[data-contador]")
+    
+    if(operacao === "-") {
+        peca.value = parseInt(peca.value) - 1
+    } else {
+        peca.value = parseInt(peca.value) + 1
+    }
+}
+
